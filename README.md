d917c283e15a4a52934e87e2c1d8ae0f401b9166
MEENIGA VENKATA RAJU KUMAR <117989109+RajuKumar0108@users.noreply.github.com>
2024-04-02 15:29:18 +0530

    basic

--- README.md
+++
@@ -1,2 +0,0 @@
-# first-development-
-My first testing repo
---
+++ demo.js
@@ -0,0 +1,32 @@
+//String.
+let a="rajukumar"
+console.log(a)
+//Number.
+let b=10
+let c=20
+let d=a+b
+console.log(d);
+//Boolean.
+let raj=true
+console.log(raj);
+//Undefined.
+let r=""
+console.log(r);
+//Null.
+let y=null
+console.log(y)
+//Aerry.
+let arry={
+    "name":"Rajukumar",
+    "number":6303825897,
+}
+console.log(arry);
+//Object
+let obj=[
+    'name',"rajukumar",
+    'number',630382597,
+    'village',"cumbum"
+]
+console.log(obj);
+
+
